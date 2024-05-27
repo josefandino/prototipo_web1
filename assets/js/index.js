1b30ab46735ade93769d82540cfef633bc6b4f51
josefandino <josefandinod@gmail.com>
2024-05-26 20:12:43 -0500

    Refactor menu item click event handling and add smooth scrolling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,40 @@
-var menuItems = document.querySelectorAll('#menu li a');
+// var menuItems = document.querySelectorAll('#menu li a');
 
-menuItems.forEach(function(item) {
-  item.addEventListener('click', function() {
-    menuItems.forEach(function(item) {
-      item.parentNode.classList.remove('active');
+// menuItems.forEach(function(item) {
+//   item.addEventListener('click', function() {
+//     menuItems.forEach(function(item) {
+//       item.parentNode.classList.remove('active');
+//     });
+//     this.parentNode.classList.add('active');
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rowLeft = document.getElementById('rowLeft');
+  const rowRight = document.getElementById('rowRight');
+  const menu = document.getElementById('menu');
+  console.log('menu', menu);
+
+  // Initially hide rowRight
+  rowRight.querySelector('img').classList.add('hidden');
+
+  rowLeft.addEventListener('click', () => {
+    menu.scrollTo({
+      top: 0,
+      behavior: 'smooth'
     });
-    this.parentNode.classList.add('active');
+    rowLeft.querySelector('img').classList.add('hidden');
+    rowRight.querySelector('img').classList.remove('hidden');
+  });
+
+  rowRight.addEventListener('click', () => {
+    menu.scrollTo({
+      top: menu.scrollHeight,
+      behavior: 'smooth'
+    });
+    rowRight.querySelector('img').classList.add('hidden');
+    rowLeft.querySelector('img').classList.remove('hidden');
   });
 });
+
+
